a28178cbd47c76fdc2c2c5e4023c70d3911a5069
gaofan <1849121296@qq.com>
2020-03-15 00:51:05 +0800

    更新

--- 静态成员02.cpp
+++ 静态成员02.cpp
@@ -2,9 +2,25 @@
 #include<string>
 using namespace std;
 
+class Goat
+{
+private:
+
+public:
 
 
 
+};
 
-
-
+int main()
+{
+    Goat g1("Pleasant Goat"), g2("Pretty Goat"), g3("Athlect Goat");
+    g2.visit("the pavilion", "having a romance", g3);
+    Goat g4("Lazy Goat");
+    g4.visit("the library", "studying");
+    Goat g5("Slow Goat"), g6("Warm Goat");
+    g2.visit("Zhongshan street", "shopping", g5);
+    g1.visit("Nanputuo Temple", "chanting scriptures");
+    g6.visit("the gymnasium", "doing physical exercise");
+    return 0;
+}
