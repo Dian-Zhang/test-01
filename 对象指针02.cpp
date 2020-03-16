f2ae3ec761154af87dccd11c5cb50fcea56f0e27
gaofan <1849121296@qq.com>
2020-03-16 18:19:46 +0800

    更新

--- 对象指针02.cpp
+++ 对象指针02.cpp
@@ -1,12 +1,60 @@
-
-
-
-
-
-
-
-
-int main(£©
+#include<iostream>
+#include<string>
+using namespace std;
+class Goat
+{
+private:
+    string name;
+    int weight;
+public:
+    Goat(string name, int weight);
+    void sleep();
+    string getName() { return name; }
+    int getWeight() { return weight;}
+};
+class Wolf
+{
+private:
+    string name;
+    int weight;
+public:
+    Wolf(string name, int weight);
+    void eat(Goat* g);
+    string getName() { return name; }
+    int getWeight() { return weight; }
+};
+Goat::Goat(string name, int weight)
+{
+    this->name = name;
+    this->weight = weight;
+}
+void Goat::sleep()
+{
+    cout << name << " has a good sleep, increasing 2 kg." << endl;
+    weight += 2;
+}
+Wolf::Wolf(string name, int weight)
+{
+    this->name = name;
+    this->weight = weight;
+}
+void Wolf::eat(Goat* g)
+{
+    cout << name << " eats " << g->getName
+        << ", increasing " << g->getWeight << endl;
+    weight += g->getWeight;
+}
+void print(Goat* g)
+{
+    cout << g->getName << " is a goat that weights "
+        << g->getWeight << " kg." << endl;
+}
+void print(Wolf* w)
+{
+    cout << w->getName << " is a wolf that weights "
+        << w->getWeight << " kg." << endl;
+}
+int main()
 {
     Goat* g1 = new Goat("Pretty Goat", 47);
     Goat* g2 = new Goat("Warm Goat", 60);
@@ -20,3 +68,4 @@ int main(£©
     print(w1);
     return 0;
 }
+
